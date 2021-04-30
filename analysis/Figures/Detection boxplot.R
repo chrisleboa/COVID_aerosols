e0d48f231d1321af92ac0e8ea677e5cb0a58b2fd
Caitlin Hemlock <hemlock_caitlin@berkeley.edu>
2021-04-29 17:31:32 -0700

    new boxplot figure

--- analysis/Figures/Detection boxplot.R
+++ analysis/Figures/Detection boxplot.R
@@ -1,5 +1,8 @@
 library(ggplot2)
 library(dplyr)
+library(cowplot)
+library(gridExtra)
+library(ggpubr)
 
 #import data
 source(here::here("configuration.R"))
@@ -10,11 +13,36 @@ d <- d %>%
   mutate(ventratelog10 = log10(ventrateavg),
          Qlog10 = log10(Q)) 
 
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
 #sample detection
-ggplot(data = d, aes(x = result, y = Qlog10)) + 
-  geom_boxplot(outlier.shape = NA) + geom_jitter(aes(color = covidspace)) +
+q <- ggplot(data = d, aes(x = result, y = Qlog10)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color = covidspace)) +
+  scale_color_manual(values = c("dodgerblue4", "darkorange2")) +
   labs(x = "qPCR RNA detection", y = "Absolute ventilation (log10 L/s)", 
        color = "COVID designation \n for sampling space") +
-  theme_minimal() +
-  theme(legend.title = element_text(hjust = -1, size = 10),
-        legend.position = "top")
+  theme_bw() +
+  theme(legend.title = element_text(hjust = -1, size = 9),
+        legend.position = "right")
+
+legend <- get_legend(q)
+q <- q + theme(legend.position = "none")
+
+ventrate <- ggplot(data = d, aes(x = result, y = ventratelog10)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color = covidspace)) +
+  scale_color_manual(values = c("dodgerblue4", "darkorange2")) +
+  labs(x = "qPCR RNA detection", y = "Ventilation (log10 L/s/p)", 
+       color = "COVID designation \n for sampling space") +
+  theme_bw() +
+  theme(legend.title = element_text(hjust = -1, size = 9),
+        legend.position = "none")
+
+ggarrange(q, ventrate, legend, nrow = 1, widths=c(2.3, 2.3, 0.8))
+
