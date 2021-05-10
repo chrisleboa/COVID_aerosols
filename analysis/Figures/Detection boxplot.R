ed289d42665db424c83ed76acd13812c1f49a1b8
Caitlin Hemlock <hemlock_caitlin@berkeley.edu>
2021-05-10 12:18:56 -0700

    change text size

--- analysis/Figures/Detection boxplot.R
+++ analysis/Figures/Detection boxplot.R
@@ -28,8 +28,11 @@ q <- ggplot(data = d, aes(x = result, y = Qlog10)) +
   labs(x = "qPCR RNA detection", y = "Absolute ventilation (log10 L/s)", 
        color = "COVID designation \n for sampling space") +
   theme_bw() +
-  theme(legend.title = element_text(hjust = -1, size = 9),
-        legend.position = "right")
+  theme(legend.title = element_text(hjust = -1, size = 13),
+        legend.position = "right",
+        legend.text = element_text(size = 13),
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 13))
 
 legend <- get_legend(q)
 q <- q + theme(legend.position = "none")
@@ -38,11 +41,16 @@ ventrate <- ggplot(data = d, aes(x = result, y = ventratelog10)) +
   geom_boxplot(outlier.shape = NA) + 
   geom_jitter(aes(color = covidspace)) +
   scale_color_manual(values = c("dodgerblue4", "darkorange2")) +
-  labs(x = "qPCR RNA detection", y = "Ventilation (log10 L/s/p)", 
+  labs(x = "qPCR RNA detection", y = "Ventilation Rate (log10 L/s/p)", 
        color = "COVID designation \n for sampling space") +
   theme_bw() +
-  theme(legend.title = element_text(hjust = -1, size = 9),
-        legend.position = "none")
+  theme(legend.title = element_text(hjust = -1, size = 13),
+        legend.position = "none",
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 13))
 
 ggarrange(q, ventrate, legend, nrow = 1, widths=c(2.3, 2.3, 0.8))
+
+ggsave(filename = "~/Documents/COVID/detection boxplot.jpg",
+       width = 11, height = 6)
 
